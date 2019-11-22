a3fb02c9c0ff13bbaec9e6271d9cdf10757b00f2
Rafael Neves Gonzaga <rafaelnevesgonzaga@gmail.com>
2019-11-22 16:54:40 -0300

    Layouts

---
+++ Seção 07/LayoutXF/LayoutXF/LayoutXF/Layout/Grid/GridPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutXF.Layout.Grid
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GridPage : ContentPage
+    {
+        public GridPage()
+        {
+            InitializeComponent();
+            GridD.ColumnDefinitions.Add(new ColumnDefinition{ Width = 150 });
+            GridD.RowDefinitions.Add(new RowDefinition { Height = 100 });
+            GridD.Children.Add(new BoxView { BackgroundColor = Color.Black, 
+                                             HorizontalOptions = LayoutOptions.FillAndExpand,
+                                             VerticalOptions = LayoutOptions.FillAndExpand, },3,3);
+
+        }
+    }
+}
---
+++ Seção 07/LayoutXF/LayoutXF/LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LayoutXF
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoAbsolute(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layout.Absolute.AbsolutePage());
+        }
+        private void GoGrid(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layout.Grid.GridPage());
+        }
+        private void GoRelative(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layout.Relative.RelativePage());
+        }
+        private void GoScroll(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layout.Scroll.ScrollPage());
+        }
+        private void GoStack(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Layout.Stack.StackPage());
+        }
+    }
+}
