3bd8d0e900bc5723983f337c60b5694f4afb37b9
Rafael Neves Gonzaga <rafaelnevesgonzaga@gmail.com>
2019-12-06 16:23:44 -0300

    CellProject_driado

---
+++ CellApp/CellApp/CellApp/CellApp/Menu/Menu.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using CellApp.Pages;
+namespace CellApp.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : MasterDetailPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void GoTextCell(object sender, EventArgs e)
+        {
+
+            Detail = new NavigationPage(new TextCellPage());
+        }
+    }
+}
---
+++ CellApp/CellApp/CellApp/CellApp/Pages/TextCellPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using CellApp.Modelo;
+namespace CellApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TextCellPage : ContentPage
+    {
+        public TextCellPage()
+        {
+            InitializeComponent();
+            List<Funcionario> lista = new List<Funcionario>();
+            lista.Add(new Funcionario() { Nome = "Rafael Neves", Cargo = "Pedreiro", Foto = "Foto" });
+            ListaFuncionario.ItemsSource = lista;
+
+        }
+    }
+}
