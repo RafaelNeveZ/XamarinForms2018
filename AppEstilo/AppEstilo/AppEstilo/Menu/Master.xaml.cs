340080033bb7f7c9cb4846dd6385722ca93f8af1
Rafael Neves Gonzaga <rafaelnevesgonzaga@gmail.com>
2019-12-09 17:02:25 -0300

    Styles_CINEMIX

---
+++ AppEstilo/AppEstilo/AppEstilo/Menu/Master.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppEstilo.Menu
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Master : MasterDetailPage
+    {
+        public Master()
+        {
+            InitializeComponent();
+        }
+
+        private void GoImplicitStyle(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page.ImplicitStylePage());
+            IsPresented = false;
+        }
+
+        private void GoExplicttStyle(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page.ExplicitStylePage());
+            IsPresented = false;
+        }
+        private void GoGlobalStylePage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page.GlobalStylePage());
+            IsPresented = false;
+        }
+        private void GoHerancaPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page.HerancaPage());
+            IsPresented = false;
+        }
+        private void GoDynamcyPage(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page.DynamcyPage());
+            IsPresented = false;
+        }
+    }
+}
---
+++ AppEstilo/AppEstilo/AppEstilo/Page/DynamcyPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppEstilo.Page
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DynamcyPage : ContentPage
+    {
+        public DynamcyPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            this.Resources["LblColor"] = Color.Magenta;
+            this.Resources["Lbl"] = this.Resources["NewLbl"];
+
+        }
+    }
+}
